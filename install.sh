e68e1f1bd369256146e08bd874884cce631299bd
ironhan <83115553+JongguHan@users.noreply.github.com>
2022-08-26 17:50:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-#prevent error
-#sudo rpm -Uvh https://download.docker.com/linux/centos/8/x86_64/stable/Packages/containerd.io-1.4.6-3.1.el8.x86_64.rpm
+
 # Auto Script For Teslamate
 curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/centos/docker-ce.repo" -o docker-ce.repo
 sudo yum-config-manager --add-repo ./docker-ce.repo
@@ -14,7 +13,7 @@ sudo systemctl enable docker
 sudo docker --version
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s -f /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 curl -L "https://raw.githubusercontent.com/JongguHan/teslamate/main/docker-compose.yml" -o docker-compose.yml
 sudo docker-compose up -d
